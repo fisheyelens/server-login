ede196fbf6b9bcf19bf17178eef3cd49278f290d
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-11 22:42:13 +0900

    made server

--- work-1.js
+++ work-1.js
@@ -1,1 +1,42 @@
-console.log("This is work-1.js");
+// * 서버 구축하는 페이지!
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function(request, response){
+  if(request.method === "GET") {
+    if(request.url === "/") {
+
+      const first = fs.readFileSync("./public/index.html", "utf8");
+
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/html; charset=utf-8');
+      response.write(first);
+      response.end();
+    }
+  
+    if(request.url === "/style.css") {
+      
+      const second = fs.readFileSync("./public/style.css");
+      
+      response.statusCode = 200;
+      response.setHeader('Content-Type', '; charset=utf-8');
+      response.write(second);
+      response.end();
+    }
+
+      if(request.url === "/index.js") {
+
+      const third = fs.readFileSync("./public/index.js");
+
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/javascript; charset=utf-8');
+      response.write(third);
+      response.end();
+    }
+    console.log(request.url);
+  }
+});
+server.listen(3000);
+
+
