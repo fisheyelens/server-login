2031bc61681227016e1cb02dd7d1930ee0ab1fc2
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-12 09:40:40 +0900

    fixed file name

--- work-1.js
+++ work-1.js
@@ -7,7 +7,7 @@ const server = http.createServer(function(request, response){
   if(request.method === "GET") {
     if(request.url === "/") {
 
-      const first = fs.readFileSync("./public/index.html", "utf8");
+      const first = fs.readFileSync("./server-login/index.html", "utf8");
 
       response.statusCode = 200;
       response.setHeader('Content-Type', 'text/html; charset=utf-8');
@@ -17,7 +17,7 @@ const server = http.createServer(function(request, response){
   
     if(request.url === "/style.css") {
       
-      const second = fs.readFileSync("./public/style.css");
+      const second = fs.readFileSync("./server-login/style.css");
       
       response.statusCode = 200;
       response.setHeader('Content-Type', '; charset=utf-8');
@@ -25,9 +25,9 @@ const server = http.createServer(function(request, response){
       response.end();
     }
 
-      if(request.url === "/index.js") {
+      if(request.url === "/work-1.js") {
 
-      const third = fs.readFileSync("./public/index.js");
+      const third = fs.readFileSync("./server-login/work-1.js");
 
       response.statusCode = 200;
       response.setHeader('Content-Type', 'text/javascript; charset=utf-8');
@@ -37,6 +37,5 @@ const server = http.createServer(function(request, response){
     console.log(request.url);
   }
 });
-server.listen(3000);
-
+server.listen(8024);
 
